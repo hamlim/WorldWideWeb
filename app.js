3267a8d47613573f078bf1aeebec2bbeff6c8af6
Matt Hamlin <hamlim@outlook.com>
2016-10-22 19:55:18 -0400

    Proper SVG scaling

--- app.js
+++ app.js
@@ -12,26 +12,26 @@ let subtitles = [
 
 
 let posts = [
-  {
-    title: 'I am a title',
-    slug: 'test',
-    id: 1,
-    lead: 'I am the article lead for this post, this should bring people in to read more about it!',
-    paragraphs: [
-      'I am some content!',
-      'I am another paragraph!'
-    ]
-  },
-  {
-    title: 'I am another Title!',
-    slug: 'testing',
-    id: 2,
-    lead: 'I am the article lead for this post, this should bring people in to read more about it!',
-    paragraphs: [
-      'I am some more content!',
-      'I am another full paragraph with a ton of useful content!'
-    ]
-  }
+  // {
+  //   title: 'I am a title',
+  //   slug: 'test',
+  //   id: 1,
+  //   lead: 'I am the article lead for this post, this should bring people in to read more about it!',
+  //   paragraphs: [
+  //     'I am some content!',
+  //     'I am another paragraph!'
+  //   ]
+  // },
+  // {
+  //   title: 'I am another Title!',
+  //   slug: 'testing',
+  //   id: 2,
+  //   lead: 'I am the article lead for this post, this should bring people in to read more about it!',
+  //   paragraphs: [
+  //     'I am some more content!',
+  //     'I am another full paragraph with a ton of useful content!'
+  //   ]
+  // }
 ];
 let count = posts.length;
 
